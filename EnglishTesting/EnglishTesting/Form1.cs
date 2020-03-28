7f433463ac875d5060aea1f4006a061056b7f124
Ruslan Rozumnyi <55786258+Rozumrus37@users.noreply.github.com>
2020-03-28 20:02:45 +0200

    Add files via upload

---
+++ EnglishTesting/EnglishTesting/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace EnglishTesting
+{
+    public partial class Form1 : Form
+    {
+        Point coordinates;
+        public string[] images = { "Photos/1.png", "Photos/2.png", "Photos/3.png", "Photos/4.png", "Photos/5.png", "Photos/6.png" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            customPictureBox1.Image = Image.FromFile(images[5]);
+
+
+
+            coordinates = customPictureBox1.Location;
+            
+        }
+    }
+}
